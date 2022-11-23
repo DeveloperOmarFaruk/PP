4de08bdb7c99c4dfa215a1117a8fc78754a39823
Md. Masum Reza <masum@metaii.com>
2022-11-24 00:52:04 +0600

    PP-01: graph data show from api

--- src/ProteinLab/PLabAnalysis/ApexChart.js
+++ src/ProteinLab/PLabAnalysis/ApexChart.js
@@ -1,71 +1,142 @@
-import React from 'react'
-import "./PLabAnalysis.css"
-import ReactApexChart from 'react-apexcharts'
-
+import React from "react";
+import "./PLabAnalysis.css";
+import ReactApexChart from "react-apexcharts";
+import axios from "axios";
 
 export default class ApexChart extends React.Component {
-    constructor(props) {
-      super(props);
+	constructor(props) {
+		super(props);
+		this.state = {
+			series: [
+				{
+					id: "spike",
+					name: "M Protein",
+					data: []
+				},
+				{
+					id: 2,
+					name: "N Protein",
+					data: []
+				},
+				{
+					id: 3,
+					name: "E Protein",
+					data: []
+				},
+				{
+					id: 4,
+					name: "Protein 4",
+					data: []
+				},
+				{
+					id: 5,
+					name: "Protein 5",
+					data: []
+				}
+			],
+			options: {
+				chart: {
+					height: 450,
+					type: "line",
+					zoom: {
+						enabled: false,
+					},
+				},
+				dataLabels: {
+					enabled: false,
+				},
+				stroke: {
+					curve: "straight",
+				},
+				// title: {
+				//   text: 'Product Trends by Month',
+				//   align: 'left'
+				// },
+				grid: {
+					row: {
+						colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+						opacity: 0.5,
+					},
+				},
+				xaxis: {
+					categories: [
+						"50",
+						"100",
+						"150",
+						"200",
+						"250",
+						"300",
+						"350",
+						"400",
+						"450",
+						"500",
+					],
+				},
+			},
+		};
+	}
 
-      this.state = {
-      
-        series: [{
-            name: "M Protein",
-            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-          },
-          {
-            name: "N Protein",
-            data: [14, 40, 55, 61, 79, 82, 99, 101, 358]
-              },
-              {
-                name: "E Protein",
-                data: [20, 46, 55, 66, 88, 44, 77, 110, 128]
-              },
-             ],
-        options: {
-          chart: {
-            height: 450,
-            type: 'line',
-            zoom: {
-              enabled: false
-            }
-          },
-          dataLabels: {
-            enabled: false
-          },
-          stroke: {
-            curve: 'straight'
-          },
-          // title: {
-          //   text: 'Product Trends by Month',
-          //   align: 'left'
-          // },
-          grid: {
-            row: {
-              colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-              opacity: 0.5
-            },
-          },
-          xaxis: {
-            categories: ['50', '100', '150', '200', '250', '300', '350', '400', '450',],
-          }
-        },
-      
-      
-      };
-    }
+	async componentDidMount() {
+		const data = {
+			region: 0,
+			lowPosition: 1,
+			highPosition: 20,
+		};
+		const a = axios.post('https://protein.catkinsofttech-bd.xyz/api/filter/spike-protein-lab-graph', data)
+		const b = axios.post('https://protein.catkinsofttech-bd.xyz/api/filter/protein-2-lab-graph', data)
+		const c = axios.post('https://protein.catkinsofttech-bd.xyz/api/filter/protein-3-lab-graph', data)
+		const d = axios.post('https://protein.catkinsofttech-bd.xyz/api/filter/protein-4-lab-graph', data)
+		const e = axios.post('https://protein.catkinsofttech-bd.xyz/api/filter/protein-5-lab-graph', data)
 
-  
-
-    render() {
-      return (
-        
-
-  <div id="chart">
-<ReactApexChart options={this.state.options} series={this.state.series} type="line" height={350} />
-</div>
-
-
-      );
-    }
-  }
+		await axios.all([a, b, c, d, e]).then(axios.spread((...responses) => {
+			const responseOne = responses[0];
+			const responseTwo = responses[1];
+			const responesThree = responses[2];
+			const responseFour = responses[3];
+			const responesFive = responses[4];
+			this.setState({
+				series: [
+					{
+						id: "spike",
+						name: "M Protein",
+						data: responseOne.data.graph_data,
+					},
+					{
+						id: 2,
+						name: "N Protein",
+						data: responseTwo.data.graph_data,
+					},
+					{
+						id: 3,
+						name: "E Protein",
+						data: responesThree.data.graph_data,
+					},
+					{
+						id: 4,
+						name: "Protein 4",
+						data: responseFour.data.graph_data,
+					},
+					{
+						id: 5,
+						name: "Protein 5",
+						data: responesFive.data.graph_data,
+					},
+				]
+			});
+		})).catch(errors => {
+			console.log("errors----", errors);
+		})
+	}
+	render() {
+		return (
+			<div id="chart">
+				<ReactApexChart
+					options={this.state.options}
+					series={this.state.series}
+					type="line"
+					height={350}
+				/>
+			</div>
+		);
+	}
+}
